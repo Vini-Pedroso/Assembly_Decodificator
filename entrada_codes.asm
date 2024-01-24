bd9a2243b47e2017c81292dd44dbcffce0c3a90c
VInicius Freitas Pedroso <129963034+Vini-Pedroso@users.noreply.github.com>
2024-01-23 22:00:55 -0300

    Create entrada_codes.asm

---
+++ entrada_codes.asm
@@ -0,0 +1,14 @@
+.text
+.globl main
+main:
+or $t1, $s3, $s4
+label_0:	or $t1, $s3, $s4
+or $t1, $s3, $s4
+sw $t0, 0($t2)
+ beq $s5, $zero, label_0
+label_1:	sw $t0, 0($t2)
+or $t1, $s3, $s4
+or $t1, $s3, $s4
+or $t1, $s3, $s4
+or $t1, $s3, $s4
+j label_1
